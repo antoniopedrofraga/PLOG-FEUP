a343d037c086dcdb26e16f224d6e2977ebb70a8c
pedrofraga05 <proteck5.pf@gmail.com>
2015-10-06 17:26:07 +0100

    Listas e tabuleiro, falta mostrar elementos da lista!

--- proj/proj.pl
+++ proj/proj.pl
@@ -1,36 +1,91 @@
-jogador(jogador1).
-jogador(jogador2).
-coluna(A, B, C, D, E, F, G).
-tabuleiro(coluna, coluna, coluna, coluna, coluna, coluna, coluna).
-helloWorld:-write('Hello World!').
+%--------------------------------%
+%--------Syrtis em ProLog--------%
+%--------------------------------%
+%------- escreve syrtis. --------%
+%---- na consola para correr ----%
+%--------------------------------%
+%--------------------------------%
 
-imprimeNLinha(X, Y) :-
-		X == 0, write('\n'), write(Y).
+
+syrtis :-
+	%criaTabuleiro(B, 0, 7)%
+	imprimeTabuleiro(0, 7).
+
+	
+%-------------------------------%
+%-------------------------------%
+%-----Predicados para criar-----%
+%----------tabuleiro------------%
+%-------------------------------%
+%-------------------------------%
+
+criaTabuleiro(A, 7, 7).
+criaTabuleiro([H | T], X, S) :-
+	X1 is X + 1,
+	criaLista(H, 0, S),
+	criaTabuleiro(T, X1, S).
+
+criaLista(A, 7, 7).
+criaLista([H | T], X, S) :-
+	X1 is X + 1,
+	H = '*',
+	criaLista(T, X1, S).
 	
 	
-imprimeLinha(X, Y) :-
-		X < 7,
-		imprimeNLinha(X, Y),
-		B is X + 1,
-		imprimeLinha(B, Y).
-
-imprimeNColunas(X, Y) :-
-	X == 0, Y < 7,
-	write(' | '),write(Y),
-	N is Y + 1,
-	imprimeNColunas(X, N).
-
-imprimeSeparador(X) :-
+	
+%------------------------------%
+%---Predicados para imprimir---%
+%------------------------------%
+imprimeLinha(0, 0, S) :-
+	write('   | '),
+	write('0'),
+	imprimeLinha(0, 1, S).
+	
+imprimeLinha(0, X, S) :-
 	X < 7,
+	write(' | '), write(X),
+	N is X + 1,
+	imprimeLinha(0, N, S).
+
+imprimeLinha(0, 7, S) :-
+	write(' |\n').
+
+imprimeLinha(Y, 7, S) :-
+	write('\n').	
+	
+imprimeLinha(Y, 0, S) :-
+	write(' '),
+	write(Y),
+	write(' |'),
+	write(' * |'),
+	imprimeLinha(Y, 1, S).
+
+imprimeLinha(Y, X, S) :- 
+	X < 7,
+	write(' * '),
+	write('|'),
+	A is X + 1,
+	imprimeLinha(Y, A, S).
+
+imprimeLinha(7, 7, S) :-
+	write('\n').
+
+	
+imprimeSeparador(X, S) :-
+	X < S,
 	write(----),
 	A is X + 1,
-	imprimeSeparador(A).
+	imprimeSeparador(A, S).
 
+imprimeSeparador(A, A):-
+	write('\n').
 
-
-imprimeTabuleiro(X, Y):-
-	Y < 7,
-	(imprimeNColunas(X, 0); (write('\n'), imprimeSeparador(0));(write('|  '),imprimeLinha(0, Y)); 1 == 1), 
-	B is Y + 1,
-	A is X + 1,
-	imprimeTabuleiro(A, B).
+imprimeTabuleiro(X, S):-
+	X < S,
+	imprimeLinha(X, 0, S),
+	imprimeSeparador(-1, S), %--desde o indice -1, para cobrir os indices das linhas--% 
+	B is X + 1,
+	imprimeTabuleiro(B, S).
+	
+imprimeTabuleiro(S,S).
+	
