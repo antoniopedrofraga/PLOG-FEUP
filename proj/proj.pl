401fdbb10f6faadef31b1897110426c39ce28d44
pedrofraga05 <proteck5.pf@gmail.com>
2015-10-06 19:30:17 +0100

    aula 06/10

--- proj/proj.pl
+++ proj/proj.pl
@@ -8,8 +8,12 @@
 
 
 syrtis :-
-	%criaTabuleiro(B, 0, 7)%
-	imprimeTabuleiro(0, 7).
+	B = _,
+	criaTabuleiro(B, 0, 7),
+	write('\n\n'),
+	imprimeTabuleiro(B, 0, 7).
+
+	
 
 	
 %-------------------------------%
@@ -19,55 +23,65 @@ syrtis :-
 %-------------------------------%
 %-------------------------------%
 
-criaTabuleiro(A, 7, 7).
+criaTabuleiro(_, A, A).
 criaTabuleiro([H | T], X, S) :-
+	X \= S,
 	X1 is X + 1,
 	criaLista(H, 0, S),
 	criaTabuleiro(T, X1, S).
 
-criaLista(A, 7, 7).
+criaLista(_, A, A).
 criaLista([H | T], X, S) :-
+	X \= S,
 	X1 is X + 1,
-	H = '*',
+	H = ' ',
 	criaLista(T, X1, S).
-	
-	
-	
+
+
 %------------------------------%
-%---Predicados para imprimir---%
+%--Predicados para vizualizar--%
+%-----------tabuleiro----------%
 %------------------------------%
-imprimeLinha(0, 0, S) :-
+
+imprimeNCol(0, 0, S) :-
 	write('   | '),
-	write('0'),
-	imprimeLinha(0, 1, S).
+	write('0 | '),	
+	imprimeNCol(0, 1, S).
 	
-imprimeLinha(0, X, S) :-
-	X < 7,
-	write(' | '), write(X),
-	N is X + 1,
-	imprimeLinha(0, N, S).
+imprimeNCol(0, Y, S) :-
+	Y < S,
+	write(Y),
+	write(' | '),
+	Y1 is Y + 1,
+	imprimeNCol(0, Y1, S).
 
-imprimeLinha(0, 7, S) :-
-	write(' |\n').
+imprimeNCol(0, S, S) :- 
+	write('\n'),
+	imprimeSeparador(-1, S).
 
-imprimeLinha(Y, 7, S) :-
+imprimeNCol(_, _, _).
+
+
+imprimeLinha(_,_, S, S) :-
 	write('\n').	
 	
-imprimeLinha(Y, 0, S) :-
+imprimeLinha([H | T], Y, 0, S) :-
 	write(' '),
 	write(Y),
+	write(' | '),
+	write(H),
 	write(' |'),
-	write(' * |'),
-	imprimeLinha(Y, 1, S).
+	imprimeLinha(T, Y, 1, S).
 
-imprimeLinha(Y, X, S) :- 
-	X < 7,
-	write(' * '),
-	write('|'),
+imprimeLinha([H | T], Y, X, S) :- 
+	X < S,
+	write(' '),
+	write(H),
+	write(' |'),
 	A is X + 1,
-	imprimeLinha(Y, A, S).
+	imprimeLinha(T, Y, A, S).
 
-imprimeLinha(7, 7, S) :-
+imprimeLinha(_, A, A, _) :-
 	write('\n').
 
 	
@@ -80,12 +94,13 @@ imprimeSeparador(X, S) :-
 imprimeSeparador(A, A):-
 	write('\n').
 
-imprimeTabuleiro(X, S):-
+imprimeTabuleiro([H | T], X, S):-
 	X < S,
-	imprimeLinha(X, 0, S),
-	imprimeSeparador(-1, S), %--desde o indice -1, para cobrir os indices das linhas--% 
-	B is X + 1,
-	imprimeTabuleiro(B, S).
+	imprimeNCol(X, 0, S),
+	imprimeLinha(H, X, 0, S),
+	imprimeSeparador(-1, S), %--desde o indice -1, para cobrir os numeros das linhas--% 
+	X1 is X + 1,
+	imprimeTabuleiro(T, X1, S).
 	
-imprimeTabuleiro(S,S).
+imprimeTabuleiro(_, S, S).
 	
